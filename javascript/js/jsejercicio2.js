04dd533fb3ca58de0c46b6109b6c72f33a3aa159
Jona2909 <j29s09c03@gmail.com>
2024-04-17 10:14:25 -0400

    commit 4

--- javascript/js/jsejercicio2.js
+++ javascript/js/jsejercicio2.js
@@ -1,12 +1,10 @@
-function sumarNumeros() {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
+
+    const num1 = parseFloat(prompt("Ingrese el primer número:"));
+    const num2 = parseFloat(prompt("Ingrese el segundo número:"));
     
     if (isNaN(num1) || isNaN(num2)) {
-        console.log("Por favor ingresa números válidos.");
-        return;
+        console.log("Por favor ingrese números válidos.");
+    } else {
+        const suma = num1 + num2;
+        console.log("La suma de " + num1 + " y " + num2 + " es: " + suma);
     }
-
-    const suma = num1 + num2;
-    console.log("La suma de " + num1 + " y " + num2 + " es: " + suma);
-}
